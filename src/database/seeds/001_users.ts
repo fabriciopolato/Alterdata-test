f8a9916f326b66210a7b2e4f3ab7e2148b310ef1
Fabricio Polato <fabriciopolato16@yahoo.com.br>
2020-07-28 11:32:48 -0300

    feat: insert crypted password in seeds

--- src/database/seeds/001_users.ts
+++ src/database/seeds/001_users.ts
@@ -1,12 +1,20 @@
 import * as Knex from 'knex';
+import bcrypt from 'bcryptjs';
 
 export async function seed(knex: Knex): Promise<void> {
   // Deletes ALL existing entries
   await knex('users').del();
 
   // Inserts seed entries
+  const salt = await bcrypt.genSalt(10);
+
+  const promise1 = bcrypt.hash('1212', salt);
+  const promise2 = bcrypt.hash('1212', salt);
+
+  const [hash1, hash2] = await Promise.all([promise1, promise2]);
+
   await knex('users').insert([
-    { email: 'joao@email.com' },
-    { email: 'carlos@email.com' }
+    { email: 'joao@email.com', password: hash1 },
+    { email: 'carlos@email.com', password: hash2 }
   ]);
 }
