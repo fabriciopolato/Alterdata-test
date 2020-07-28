97767f6753cbb44418160c7e8e2916e94793a6c2
Fabricio Polato <fabriciopolato16@yahoo.com.br>
2020-07-28 11:30:24 -0300

    feat: add error handler in server

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,13 @@ import routes from './routes/users.routes';
 
 const app = express();
 
-app.use(routes);
+app.use(express.json());
+app.use('/', routes);
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({ error: error.message });
+});
 
 app.listen(3333, () => {
   console.log('Server is running');
