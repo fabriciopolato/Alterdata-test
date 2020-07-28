bc4b9ec16ba5ea087babfa45f7b9c6d8d32105d9
Fabricio Polato <fabriciopolato16@yahoo.com.br>
2020-07-28 13:16:05 -0300

    feat: test authorization route

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,12 +1,19 @@
 import { Router } from 'express';
 import UsersController from '@controllers/UsersController';
 import TicketsController from '@controllers/TicketsController';
+import passport from 'passport';
 
 const routes = Router();
 const usersController = new UsersController();
 const ticketsController = new TicketsController();
 
 routes.post('/users', usersController.login);
+
+routes.use(passport.authenticate('jwt', { session: false }));
+
+routes.get('/users', (req, res) => {
+  res.send('hello world');
+});
 routes.post('/tickets', ticketsController.create);
 routes.put('/tickets', ticketsController.update);
 
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,19 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import '@controllers/UsersController';
 import routes from './routes/users.routes';
+import passport from 'passport';
+import passportMiddleware from './middleware/passport';
 
 const app = express();
+
+app.use(passport.initialize());
+
+passport.use(passportMiddleware);
 
 app.use(express.json());
 app.use('/', routes);
 
-app.use((error, req, res, next) => {
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
   res.status(error.status || 500);
   res.json({ error: error.message });
 });
