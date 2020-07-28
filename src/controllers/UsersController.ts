63b1d2f6c3e3feec56f3d823da7bc77dba563a3d
Fabricio Polato <fabriciopolato16@yahoo.com.br>
2020-07-27 23:28:09 -0300

    feat: create login method in users controller

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -1,7 +1,10 @@
-import { User } from '@models/User'
+// import { User } from '@models/User';
+import knex from '@database';
 
-export class UsersController {
-  teste () {
-    const user = new User()
+export default class UsersController {
+  async login(req, res) {
+    // const user = new User();
+    const results = await knex('users');
+    return res.json(results);
   }
 }
