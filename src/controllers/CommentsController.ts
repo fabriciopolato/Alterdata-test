5befd9ddff307359d8d55f4c941d58b5ffebcfda
Fabricio Polato <fabriciopolato16@yahoo.com.br>
2020-07-30 21:03:08 -0300

    feat: create CommentsController

---
+++ src/controllers/CommentsController.ts
@@ -0,0 +1,77 @@
+import knex from '@database';
+import { Request, Response, NextFunction } from 'express';
+import { IComment, IUser } from '@interfaces';
+import { Comment } from '@models/Comment';
+
+export default class CommentsController {
+  async create(req: Request, res: Response, next: NextFunction) {
+    const { message }: IComment = req.body;
+    const ticket_id = Number(req.params.id);
+    const { id } = req.user as IUser;
+
+    if (isNaN(ticket_id) || !ticket_id) {
+      return res.status(400).json({ message: 'parâmetro inválido' });
+    }
+
+    const comment = new Comment({
+      message,
+      user_id: id,
+      ticket_id
+    });
+
+    try {
+      const [createdComment] = await knex<IComment>('comments')
+        .insert(comment)
+        .returning('*');
+
+      return res.status(201).json(createdComment);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async get(req: Request, res: Response, next: NextFunction) {
+    const ticket_id = req.params.id;
+
+    try {
+      const foundComments = await knex<IComment>('comments').where(
+        'ticket_id',
+        ticket_id
+      );
+
+      // if (foundComments.length === 0) {
+      //   return res.json({
+      //     message: 'Não existem tickets abertos para este usuário'
+      //   });
+      // }
+
+      return res.json(foundComments);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async delete(req: Request, res: Response, next: NextFunction) {
+    const ticket_id = Number(req.params.ticket_id);
+    const comment_id = Number(req.params.comment_id);
+    const { id } = req.user as IUser;
+
+    try {
+      const deletedComment = await knex<IComment>('comments')
+        .where('id', comment_id)
+        .where('ticket_id', ticket_id)
+        .where('user_id', id)
+        .del();
+
+      if (deletedComment) {
+        return res.status(200).json(deletedComment);
+      }
+      return res.status(403).json({
+        message:
+          'ticket e/ou comentário inexistentes ou usuário não está autorizado a apagar este comentário'
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+}
