09ffe41a998be0209955a1ab702ef2c666fa0abd
Fabricio Polato <fabriciopolato16@yahoo.com.br>
2020-07-30 21:05:53 -0300

    feat: add CommentsController routes

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 import UsersController from '@controllers/UsersController';
 import TicketsController from '@controllers/TicketsController';
 import passport from 'passport';
+import CommentsController from '@controllers/CommentsController';
 
 const routes = Router();
 const usersController = new UsersController();
 const ticketsController = new TicketsController();
+const commentsController = new CommentsController();
 
 routes.post('/login', usersController.login);
 
@@ -16,6 +18,13 @@ routes.get('/tickets', ticketsController.get);
 routes.get('/tickets/archived', ticketsController.getClosedTickets);
 routes.put('/tickets/:id', ticketsController.update);
 routes.delete('/tickets/:id', ticketsController.delete);
-routes.put('/tickets/reopen/:id', ticketsController.reopen);
+routes.put('/tickets/:id/reopen', ticketsController.reopen);
+
+routes.post('/tickets/:id/comments', commentsController.create);
+routes.get('/tickets/:id/comments', commentsController.get);
+routes.delete(
+  '/tickets/:ticket_id/comments/:comment_id',
+  commentsController.delete
+);
 
 export default routes;
